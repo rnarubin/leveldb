85308ff3c8b25614925630ef1be9756d8e870181
rnarubin <rnarubin@cleversafe.com>
2014-07-28 14:55:33 -0500

    optimized peek for snapshotseekingiterator

--- leveldb/src/main/java/org/iq80/leveldb/impl/SnapshotSeekingIterator.java
+++ leveldb/src/main/java/org/iq80/leveldb/impl/SnapshotSeekingIterator.java
@@ -138,9 +138,25 @@ public final class SnapshotSeekingIterator extends AbstractReverseSeekingIterato
          }
       }
 
-      //savedEntry = iterator.prev();
-
       return Maps.immutableEntry(savedEntry.getKey().getUserKey(), savedEntry.getValue());
+   }
+
+   @Override
+   protected Entry<Slice, Slice> peekInternal(){
+      if(hasNextInternal()){
+         Entry<InternalKey, Slice> peek = iterator.peek();
+         return Maps.immutableEntry(peek.getKey().getUserKey(), peek.getValue());
+      }
+      return null;
+   }
+
+   @Override
+   protected Entry<Slice, Slice> peekPrevInternal(){
+      if(hasPrevInternal()){
+         Entry<InternalKey, Slice> peekPrev = iterator.peekPrev();
+         return Maps.immutableEntry(peekPrev.getKey().getUserKey(), peekPrev.getValue());
+      }
+      return null;
    }
 
    private void findNextUserEntry(boolean skipping, Slice skipKey)
