0658ac5cbe74e9d49e9ed9e15234b60a3d33f97a
rnarubin <rnarubin@cleversafe.com>
2014-08-05 10:17:20 -0500

    removed proprietary headers

--- leveldb/src/main/java/org/iq80/leveldb/impl/ReverseIterator.java
+++ leveldb/src/main/java/org/iq80/leveldb/impl/ReverseIterator.java
@@ -1,21 +1,3 @@
-//
-// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
-//
-// Contact Information:
-// Cleversafe, Inc.
-// 222 South Riverside Plaza
-// Suite 1700
-// Chicago, IL 60606, USA
-//
-// licensing@cleversafe.com
-//
-// END-OF-HEADER
-//-----------------------
-// @author: renar
-//
-// Date: Jun 30, 2014
-//---------------------
-
 package org.iq80.leveldb.impl;
 
 import java.util.Iterator;
