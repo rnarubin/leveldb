61742362d5c263998ed027a89ff580b6e7c323f8
rnarubin <rnarubin@cleversafe.com>
2014-10-17 16:53:05 -0500

    add class to dump state of iterators

---
+++ leveldb/src/main/java/org/iq80/leveldb/util/IteratorDumper.java
@@ -0,0 +1,127 @@
+
+package org.iq80.leveldb.util;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.RecursiveToStringStyle;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.iq80.leveldb.DB;
+import org.iq80.leveldb.DBIterator;
+import org.iq80.leveldb.Options;
+import org.iq80.leveldb.impl.FileMetaData;
+import org.iq80.leveldb.impl.Iq80DBFactory;
+import org.iq80.leveldb.impl.SeekingIteratorAdapter;
+import org.iq80.leveldb.impl.TableCache;
+
+public class IteratorDumper
+{
+   public static void main(String[] args) throws IOException{
+      DB db = Iq80DBFactory.factory.open(FileUtils.createTempDir("leveldb"), new Options().useMMap(false));
+      DBIterator iter = db.iterator();
+      
+      System.out.println(dump(db, iter));
+      //System.out.println(prettyFormat("[a=123,b=[c=456,d=789],q=qwer,p=[],z={}]"));
+   }
+   public static String dump(DB db, DBIterator DBiter){
+      StringBuilder sb = new StringBuilder("DBIterator dump:\n");
+      if(!(db instanceof org.iq80.leveldb.impl.DbImpl) || !(DBiter instanceof org.iq80.leveldb.impl.SeekingIteratorAdapter))
+      {
+         sb.append("Not an Iq80LevelDB iterator");
+         return sb.toString();
+      }
+      
+      append(sb, getField(db, "options"));
+
+      SeekingIteratorAdapter iter = (SeekingIteratorAdapter)DBiter;
+      append(sb, iter);
+      
+      return sb.toString();
+   }
+   
+   private static Object getField(Object owner, String varName)
+   {
+      try
+      {
+         return FieldUtils.readDeclaredField(owner, varName, true);
+      }
+      catch (IllegalAccessException e){}
+      return null;
+   }
+
+   private static Object getSubField(Object owner, String vars){
+      for(String var:vars.split("\\."))
+      {
+         owner = getField(owner, var);
+      }
+      return owner;
+   }
+
+   @SuppressWarnings("serial")
+   private static final ToStringStyle toStringStyle = new RecursiveToStringStyle(){
+      {
+         this.setArrayStart("[");
+         this.setArrayEnd("]");
+         this.setContentStart("{");
+         this.setContentEnd("}");
+      }
+      HashSet<Class<?>> skipClasses = new HashSet<Class<?>>(Arrays.asList(Slice.class, TableCache.class, FileMetaData.class));
+      protected boolean accept(Class<?> clazz) {
+         return !skipClasses.contains(clazz);
+      };
+   };
+   private static void append(StringBuilder sb, Object o){
+      sb.append(ReflectionToStringBuilder.toString(o, toStringStyle)).append("\n");
+   }
+
+   public static String prettyFormat(String s)
+   {
+      StringBuilder sb = new StringBuilder();
+      StringBuilder tabs = new StringBuilder();
+      for(int i = 0; i < s.length(); i++){
+         char c = s.charAt(i);
+         switch(c){
+            case '[':
+            case '{':
+               sb.append(c);
+               if(i < s.length()-1 && s.charAt(i+1)==match(c)){ //treat [] and {} as special (lump them together, no newline)
+                  i++;
+                  sb.append(s.charAt(i));
+               }
+               else{
+                  sb.append('\n');
+                  tabs.append('\t');
+                  sb.append(tabs);
+               }
+               break;
+            case ']':
+            case '}':
+               sb.append('\n');
+               if(tabs.length() > 0) tabs.deleteCharAt(tabs.length()-1);
+               sb
+                 .append(tabs)
+                 .append(c);
+               break;
+            case ',':
+               sb.append(c).append('\n').append(tabs);
+               break;
+            default:
+               sb.append(c);
+         }
+      }
+      return sb.toString();
+   }
+   
+   private static char match(char c){
+      switch(c){
+         case '[': return ']';
+         case '{': return '}';
+         case ']': return '[';
+         case '}': return '{';
+      }
+      return '\0';
+   }
+}
