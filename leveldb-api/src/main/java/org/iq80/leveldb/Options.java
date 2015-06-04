a10360f577187a0d51448ce52621b47495209722
rnarubin <rnarubin@cleversafe.com>
2015-06-04 17:27:39 -0500

    moved data model null check to options

--- leveldb-api/src/main/java/org/iq80/leveldb/Options.java
+++ leveldb-api/src/main/java/org/iq80/leveldb/Options.java
@@ -169,14 +169,14 @@ public class Options
         }
     }
 
-    public static final int CPU_DATA_MODEL = Integer.getInteger("sun.arch.data.model");
+    public static final Integer CPU_DATA_MODEL = Integer.getInteger("sun.arch.data.model");
 
     // We only use MMAP on 64 bit systems since it's really easy to run out of
     // virtual address space on a 32 bit system when all the data is getting mapped
     // into memory. If you really want to use MMAP anyways, use -Dleveldb.mmap=true
     // or set useMMap(boolean) to true
     public static final boolean USE_MMAP_DEFAULT = Boolean.parseBoolean(System.getProperty("leveldb.mmap", ""
-            + (CPU_DATA_MODEL > 32)));
+            + (CPU_DATA_MODEL != null && CPU_DATA_MODEL > 32)));
 
     private boolean createIfMissing = true;
     private boolean errorIfExists = false;
