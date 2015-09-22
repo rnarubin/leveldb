8b0610c3ebe62f93e219b1a3787e0701db718ffc
rnarubin <rnarubin@cleversafe.com>
2015-09-22 14:41:13 -0500

    fix table cache

--- leveldb/src/main/java/org/iq80/leveldb/impl/TableCache.java
+++ leveldb/src/main/java/org/iq80/leveldb/impl/TableCache.java
@@ -1,130 +1,91 @@
 /*
- * Copyright (C) 2011 the original author or authors.
- * See the notice.md file distributed with this work for additional
- * information regarding copyright ownership.
+ * Copyright (C) 2011 the original author or authors. See the notice.md file distributed with this
+ * work for additional information regarding copyright ownership.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.iq80.leveldb.impl;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.cache.RemovalListener;
-import com.google.common.cache.RemovalNotification;
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.concurrent.CompletionStage;
 
 import org.iq80.leveldb.Env.DBHandle;
 import org.iq80.leveldb.FileInfo;
 import org.iq80.leveldb.Options;
 import org.iq80.leveldb.table.Table;
 import org.iq80.leveldb.table.TableIterator;
+import org.iq80.leveldb.util.CompletableFutures;
 
-import java.io.IOException;
-import java.lang.Thread.UncaughtExceptionHandler;
-import java.util.concurrent.ExecutionException;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 
-public final class TableCache
-        implements AutoCloseable
-{
-    private final LoadingCache<Long, Table> cache;
+public final class TableCache implements AutoCloseable {
+  private final LoadingCache<Long, CompletionStage<Table>> cache;
 
-    public TableCache(final DBHandle dbHandle,
-            int tableCacheSize,
-            final InternalKeyComparator userComparator,
-            final Options options,
-            final UncaughtExceptionHandler backgroundExceptionHandler)
-    {
-        cache = CacheBuilder.newBuilder()
-                .maximumSize(tableCacheSize)
-                .removalListener(new RemovalListener<Long, Table>()
-                {
-                    @Override
-                    public void onRemoval(RemovalNotification<Long, Table> notification)
-                    {
-                        Table table = notification.getValue();
-                        try {
-                            table.release(); // corresponding to constructor implicit retain
-                        }
-                        catch (Exception e) {
-                            backgroundExceptionHandler.uncaughtException(Thread.currentThread(), e);
-                        }
-                    }
-                })
-                .build(new CacheLoader<Long, Table>()
-                {
-                    @Override
-                    public Table load(Long fileNumber)
-                            throws IOException
-                    {
-                        return openTableFile(dbHandle, fileNumber, userComparator, options);
-                    }
+  public TableCache(final DBHandle dbHandle, final int tableCacheSize,
+      final InternalKeyComparator userComparator, final Options options,
+      final UncaughtExceptionHandler backgroundExceptionHandler) {
+    this.cache = CacheBuilder.newBuilder().maximumSize(tableCacheSize)
+        .<Long, CompletionStage<Table>>removalListener(
+            notification -> notification.getValue().whenComplete((table, openException) -> {
+              if (openException != null) {
+                // table was opened successfully
+                table.release().whenComplete((voided, closeException) -> {
+                  if (closeException != null) {
+                    backgroundExceptionHandler.uncaughtException(Thread.currentThread(),
+                        closeException);
+                  }
                 });
-    }
+              }
+            }))
+        .build(new CacheLoader<Long, CompletionStage<Table>>() {
+          @Override
+          public CompletionStage<Table> load(final Long fileNumber) {
+            return options.env().openRandomReadFile(FileInfo.table(dbHandle, fileNumber))
+                .thenCompose(file -> Table.newTable(file, userComparator, options));
+          }
+        });
+  }
 
-    public TableIterator newIterator(FileMetaData file)
-    {
-        return newIterator(file.getNumber());
-    }
+  public CompletionStage<TableIterator> newIterator(final FileMetaData file) {
+    return newIterator(file.getNumber());
+  }
 
-    public TableIterator newIterator(long number)
-    {
-        return getTable(number).iterator();
-    }
+  public CompletionStage<TableIterator> newIterator(final long number) {
+    return getTable(number).thenApply(Table::iterator);
+  }
 
-    public long getApproximateOffsetOf(FileMetaData file, InternalKey key)
-    {
-        try (Table table = getTable(file.getNumber())) {
-            return table.getApproximateOffsetOf(key);
-        }
-    }
+  public CompletionStage<Long> getApproximateOffsetOf(final FileMetaData file,
+      final InternalKey key) {
+    return CompletableFutures.composeUnconditionally(getTable(file.getNumber()), table -> {
+      final long offset = table.getApproximateOffsetOf(key);
+      return table.release().thenApply(voided -> offset);
+    });
+  }
 
-    private Table getTable(long number)
-    {
-        Table table;
-        try {
-            // minuscule chance of race between cache eviction and table release.
-            // re-read cache until we get a winner
-            while ((table = cache.get(number).retain()) == null)
-                ;
-        }
-        catch (ExecutionException e) {
-            Throwable cause = e;
-            if (e.getCause() != null) {
-                cause = e.getCause();
-            }
-            throw new RuntimeException("Could not open table " + number, cause);
-        }
-        return table;
-    }
+  private CompletionStage<Table> getTable(final long number) {
+    final CompletionStage<Table> attempt = cache.getUnchecked(number);
+    return attempt.thenCompose(
+        // recurse in the event of a race between this call's retain and an
+        // eviction-refcount-decremented dispose
+        table -> table.retain() != null ? attempt : getTable(number));
+  }
 
-    @Override
-    public void close()
-    {
-        cache.invalidateAll();
-    }
+  @Override
+  public void close() {
+    cache.invalidateAll();
+  }
 
-    public void evict(long number)
-    {
-        cache.invalidate(number);
-    }
-
-    private static Table openTableFile(DBHandle dbHandle,
-            long fileNumber,
-            InternalKeyComparator userComparator,
-            Options options)
-            throws IOException
-    {
-        return new Table(options.env().openRandomReadFile(FileInfo.table(dbHandle, fileNumber)), userComparator,
-                options);
-    }
+  public void evict(final long number) {
+    cache.invalidate(number);
+  }
 }
